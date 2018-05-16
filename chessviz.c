396ffd3e5f2593e31326acd7d02e76eb000bfe88
Ubunt <ubunt@localhost.localdomain>
2018-05-17 00:12:09 +0700

    End chessviz.c

--- chessviz.c
+++ chessviz.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main {
-printf(" 8 r n b q k b n r");
-printf(" 7 p p p p p p p p");
-printf(" 6                ");
-printf(" 5                ");
-printf(" 4                ");
-printf(" 3                ");
-printf(" 2 P P P P P P P P");
-printf(" 1 R N B Q K B N R");
+printf(" 8 r n b q k b n r\n"); 
+printf(" 7 p p p p p p p p\n");
+printf(" 6                \n");
+printf(" 5                \n");
+printf(" 4                \n");
+printf(" 3                \n");
+printf(" 2 P P P P P P P P\n");
+printf(" 1 R N B Q K B N R\n");
 printf("   a b c d e f g h");
 return 0;
 }
